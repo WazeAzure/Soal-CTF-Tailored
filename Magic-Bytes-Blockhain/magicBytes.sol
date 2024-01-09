b471b33ed665ec3a563b68c75e1244dc3fc308f0
EleanorCordelia <bryan.rich0604@gmail.com>
2024-01-09 13:43:55 +0700

    [add] magic-bytes

---
+++ Magic-Bytes-Blockhain/magicBytes.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <0.9.0;
+
+contract MagicBytes {
+    /**
+   * @dev soal ctf to leaern about function 4 bytes signature
+   */
+
+   uint private x = 0;
+   int counter = 10;
+   function awikwok_lmao() public{
+    x = 5;
+   }
+
+   function almost_there() public pure returns (string memory) {
+    return "sayang-sayang disayang. Kenapa masuk ITB :(";
+   }
+
+   function kapan_kapan() public {
+    for (int i=0; i < counter; i++) {
+        x += 1;
+    }
+   }
+
+   function nyerah_aja_dah(int a, int b) public pure returns (int temp) {
+    if(a == 0){
+        return b;
+    }
+    return nyerah_aja_dah(b % a, a);
+   }
+
+   function tH1S_iS_F74G_FunCT10n_4Byt3S_S1GN4tu123() public {
+
+   }
+
+}
